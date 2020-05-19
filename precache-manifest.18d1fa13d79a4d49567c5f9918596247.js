64cbd078f9752998777e305b382e027464d3953b
aubehub <albalopsanchez@gmail.com>
2020-05-19 20:05:06 -0300

    Updates

---
+++ precache-manifest.18d1fa13d79a4d49567c5f9918596247.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62324b0c3c1a111fb633f3752b2a697d",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "153ce77ae9e565cf9411",
+    "url": "/to-do-list/static/css/main.211a9127.chunk.css"
+  },
+  {
+    "revision": "2402116d0b4dc5df2964",
+    "url": "/to-do-list/static/js/2.34c31f1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.34c31f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153ce77ae9e565cf9411",
+    "url": "/to-do-list/static/js/main.a8e06b4d.chunk.js"
+  },
+  {
+    "revision": "fa27aabefbb81c21a734",
+    "url": "/to-do-list/static/js/runtime-main.80f3eba2.js"
+  }
+]);
